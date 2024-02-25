5948ae6775955c9c7e57ebfefb2cef940cad4721
Abdelrahman Tamer <abdelrahmantamer2005@yahoo.com>
2024-02-25 22:08:43 +0200

    Table,Page,Field classes

---
+++ src/Field.java
@@ -0,0 +1,8 @@
+public class Field{
+
+    private String columnName;
+    private String columnType;
+    private boolean clusteringKey;
+    private String indexName;
+    private String indexType;
+}
---
+++ src/Table.java
@@ -0,0 +1,6 @@
+import java.io.Serializable;
+
+public class Table implements Serializable {
+    private String name;
+    private String[] fileNames;
+}
