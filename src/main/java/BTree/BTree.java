7fc25f07268acfb5de4d0b6eeeae01c3f120dbc3
anastamerr <anastamer136@gmail.com>
2024-03-10 23:15:06 +0200

    B plus tree implementation

---
+++ src/main/java/BTree/BTree.java
@@ -0,0 +1,76 @@
+package BTree;
+
+/**
+ * A B+ tree
+ * Since the structures and behaviors between internal node and external node are different, 
+ * so there are two different classes for each kind of node.
+ * @param <TKey> the data type of the key
+ * @param <TValue> the data type of the value
+ */
+public class BTree<TKey extends Comparable<TKey>, TValue> {
+	private BTreeNode<TKey> root;
+	
+	public BTree() {
+		this.root = new BTreeLeafNode<TKey, TValue>();
+	}
+
+	/**
+	 * Insert a new key and its associated value into the B+ tree.
+	 */
+	public void insert(TKey key, TValue value) {
+		BTreeLeafNode<TKey, TValue> leaf = this.findLeafNodeShouldContainKey(key);
+		leaf.insertKey(key, value);
+		
+		if (leaf.isOverflow()) {
+			BTreeNode<TKey> n = leaf.dealOverflow();
+			if (n != null)
+				this.root = n; 
+		}
+	}
+	
+	/**
+	 * Search a key value on the tree and return its associated value.
+	 */
+	public TValue search(TKey key) {
+		BTreeLeafNode<TKey, TValue> leaf = this.findLeafNodeShouldContainKey(key);
+		
+		int index = leaf.search(key);
+		return (index == -1) ? null : leaf.getValue(index);
+	}
+	
+	/**
+	 * Delete a key and its associated value from the tree.
+	 */
+	public void delete(TKey key) {
+		BTreeLeafNode<TKey, TValue> leaf = this.findLeafNodeShouldContainKey(key);
+		
+		if (leaf.delete(key) && leaf.isUnderflow()) {
+			BTreeNode<TKey> n = leaf.dealUnderflow();
+			if (n != null)
+				this.root = n; 
+		}
+	}
+	
+	/**
+	 * Search the leaf node which should contain the specified key
+	 */
+	@SuppressWarnings("unchecked")
+	private BTreeLeafNode<TKey, TValue> findLeafNodeShouldContainKey(TKey key) {
+		BTreeNode<TKey> node = this.root;
+		while (node.getNodeType() == TreeNodeType.InnerNode) {
+			node = ((BTreeInnerNode<TKey>)node).getChild( node.search(key) );
+		}
+		
+		return (BTreeLeafNode<TKey, TValue>)node;
+	}
+
+	public static void main(String[] args){
+		BTree bTree = new BTree();
+		//123 is the value in the table while 5,12 is the pointer to the 5th page in the 12th index
+		//insert(key,value)
+		bTree.insert("123","5,12");
+		bTree.insert("12","12");
+		String data = String.valueOf(bTree.search("123"));
+		System.out.println(data);
+	}
+}
